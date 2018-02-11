8ee9047ffce62553b74934cdf658cb27ba974185
nobu <algebra.zetafunc@gmail.com>
2018-02-11 21:40:08 +0900

    [update] スレッド名を表示するようにした。

--- threadJankenGame/src/game/JankenCallable.java
+++ threadJankenGame/src/game/JankenCallable.java
@@ -5,9 +5,23 @@ import java.util.concurrent.TimeUnit;
 
 public class JankenCallable implements Callable<Janken.Hand> {
 
+	public static int playerNumber = 1;
+	public final static Object lock = new Object();
+
 	@Override
 	public Janken.Hand call() throws Exception {
 		TimeUnit.SECONDS.sleep(1);
+
+		synchronized (lock) {
+			playerNumber++;
+
+			if (playerNumber % 2 == 0) {
+				System.out.println("1人目の名前 ： " + Thread.currentThread().getName());
+			} else {
+				System.out.println("2人目の名前 ： " + Thread.currentThread().getName());
+			}
+		}
+
 		double random = (int) (Math.random() * 10);
 		if (6 < random) {
 			return Janken.Hand.GUU;
